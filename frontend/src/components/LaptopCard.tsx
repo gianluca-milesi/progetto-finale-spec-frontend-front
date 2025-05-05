857e63a322f625b9bc622b3dd32adacdc2d40789
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-05-05 13:06:49 +0200

    Update: LaptopCard style and placeholder

--- frontend/src/components/LaptopCard.tsx
+++ frontend/src/components/LaptopCard.tsx
@@ -1,3 +1,6 @@
+import placeholder from "../../public/laptop-placeholder.jpg"
+//React router dom
+import { Link } from "react-router-dom"
 //Types
 import { Laptop } from "../types/Laptop"
 
@@ -8,16 +11,18 @@ type Props = {
 
 function LaptopCard({ laptop }: Props) {
     return (
-        <div>
-            <h3>{laptop.title}</h3>
-            <p>{laptop.brand} - {laptop.category}</p>
-            <ul>
-                <li><strong>RAM:</strong> {laptop.ram} GB</li>
-                <li><strong>Storage:</strong> {laptop.storage} GB</li>
-                <li><strong>OS:</strong> {laptop.os}</li>
-                <li><strong>Anno:</strong> {laptop.releaseYear}</li>
-            </ul>
-        </div>
+        <Link to="/">
+            <div className="flex items-center shadow-2xl rounded-xl gap-2 m-4 relative hover:scale-103 duration-200 ease-in secondary-color text-white">
+                <figure>
+                    <img src={placeholder} className="w-25 rounded-l-xl" />
+                </figure>
+                <div className="flex-col p-1">
+                    <h3>{laptop.title}</h3>
+                    <p>{laptop.category}</p>
+                    <button className="button absolute right-4 bottom-2">Compara</button>
+                </div>
+            </div>
+        </Link>
     )
 }
 
