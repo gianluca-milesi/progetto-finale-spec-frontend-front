9770a8aa7c18a0a4f14629a8dad1bdc365522630
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-05-07 12:07:31 +0200

    ADD: LaptopDetailPage layout

---
+++ frontend/src/components/LaptopDetailInfo.tsx
@@ -0,0 +1,23 @@
+import { Laptop } from "../types/Laptop.ts"
+
+type Props = {
+    laptop: Laptop
+}
+
+
+function LaptopDetailInfo({ laptop }: Props) {
+    return (
+        <div className="bg-white shadow-md rounded-lg p-4">
+            <ul>
+                <li><strong>Categoria:</strong> {laptop.category}</li>
+                <li><strong>Brand:</strong> {laptop.brand}</li>
+                <li><strong>RAM:</strong> {laptop.ram} GB</li>
+                <li><strong>Storage:</strong> {laptop.storage}</li>
+                <li><strong>Sistema operativo:</strong> {laptop.os}</li>
+                <li><strong>Anno:</strong> {laptop.releaseYear}</li>
+            </ul>
+        </div>
+    )
+}
+
+export default LaptopDetailInfo
--- frontend/src/pages/LaptopDetailPage.tsx
+++ frontend/src/pages/LaptopDetailPage.tsx
@@ -1,9 +1,36 @@
-
+//React router dom
+import { useParams } from "react-router-dom"
+//Contexts
+import GlobalContext from "../contexts/GlobalContext"
+//Hooks
+import { useContext } from "react"
+//Pages
+import NotFoundPage from "./NotFoundPage"
+//Components
+import LaptopDetailInfo from "../components/LaptopDetailInfo.tsx"
+import CompareButton from "../components/CompareButton.tsx"
+import FavoriteButton from "../components/FavoriteButton.tsx"
 
 
 function LaptopDetailPage() {
+    const { id } = useParams()
+    const context = useContext(GlobalContext)
+    if (!context) return <div>Caricamento...</div>
+    const { laptops } = context
+
+    const laptop = laptops.find(l => l.id === parseInt(id || "", 10))
+    if (!laptop) return <NotFoundPage />
+
+
     return (
-        <div>Detail Page</div>
+        <div className="container max-w-xl py-6">
+            <h2 className="text-2xl font-semibold text-center mb-6">{laptop.title}</h2>
+            <LaptopDetailInfo laptop={laptop} />
+            <div className="flex gap-4 justify-center mt-6">
+                <CompareButton laptop={laptop} />
+                <FavoriteButton laptop={laptop} />
+            </div>
+        </div>
     )
 }
 
