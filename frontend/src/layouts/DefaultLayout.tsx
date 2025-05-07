1287625e6308ec531c3161912cb398516072d2f0
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-05-07 10:41:56 +0200

    ADD: React icons and Footer layout and style

--- frontend/src/layouts/DefaultLayout.tsx
+++ frontend/src/layouts/DefaultLayout.tsx
@@ -6,13 +6,13 @@ import Footer from "./Footer.tsx"
 
 function DefaultLayout() {
     return (
-        <>
+        <div className="flex flex-col min-h-screen">
             <Header />
-            <main className="primary-color py-4">
+            <main className="primary-color py-4 flex-grow">
                 <Outlet />
             </main>
             <Footer />
-        </>
+        </div>
     )
 }
 
--- frontend/src/layouts/Footer.tsx
+++ frontend/src/layouts/Footer.tsx
@@ -1,6 +1,31 @@
+//React router dom
+import { Link } from "react-router-dom"
+//React Icons
+import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from "react-icons/fa"
+
+
 function Footer() {
     return (
-        <footer className="secondary-color p-4">Footer</footer>
+        <footer className="secondary-color border-t py-4 text-sm text-gray-500">
+            <div className="container text-center">
+                <p>&copy;2025 Comparatore di Laptop. Tutti i diritti riservati.</p>
+                <p className="mb-4">Creato con 💻 per il progetto finale Boolean</p>
+                <div className="flex justify-center gap-6 text-gray-500 text-xl">
+                    <Link to="https://facebook.com" target="_blank">
+                        <FaFacebookF className="hover:text-blue-600 transition-colors" />
+                    </Link>
+                    <Link to="https://twitter.com" target="_blank">
+                        <FaTwitter className="hover:text-sky-500 transition-colors" />
+                    </Link>
+                    <Link to="https://instagram.com" target="_blank">
+                        <FaInstagram className="hover:text-pink-500 transition-colors" />
+                    </Link>
+                    <Link to="https://linkedin.com" target="_blank">
+                        <FaLinkedinIn className="hover:text-blue-700 transition-colors" />
+                    </Link>
+                </div>
+            </div>
+        </footer>
     )
 }
 
